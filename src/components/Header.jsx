b6ddd33cfdd7b21dc48391631f8d1dc31b70d7fb
Tajinder Singh Dhoot <tajindersinghdhoot@Singh-On-Air.local>
2024-01-01 20:32:15 -0500

    added header, Input and output components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header id='header'>
+        {/* {src/assets/investment-calculator-logo.png} */}
+        <img alt='logo' src='./src/assets/investment-calculator-logo.png' />
+        <h1>Investment Calculator</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Output.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Output() {
+  return (
+    <table id='result' className='center'>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>178</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>178</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+
+export default Output
